81a961cbafdb4b28a764ad1e462dea8557b52f34
majkel <michal.sobotka.84@gmail.com>
2020-07-30 22:03:00 +0200

    1.1 Test scenario Game of all misses - test passed without count score implementatio

--- bowling/player-ut.cpp
+++ bowling/player-ut.cpp
@@ -2,4 +2,10 @@
 
 #include "player.hpp"
 
-struct TestPlayer : public ::testing::Test {};
+struct TestPlayer : public ::testing::Test {
+    Player player;
+};
+
+TEST_F(TestPlayer, checkScoreGameOfAllMisses) {
+    ASSERT_EQ(player.getScore(), 0);
+}
