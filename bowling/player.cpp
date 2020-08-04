69724398de8fdaf8aa3a88fe13c796c4c69f36e1
Kasper Biegun <kasper.biegun95@gmail.com>
2020-08-04 15:59:29 +0200

    Testing scenarios 1.4, 1.5, 1.6, 1.7, 1.8 and many more - Kasperice  (#13)
    * 1.4 Testing scenario - game with strike in the middle - failed
    
    * 1.4 Testing scenario - game with strike in the middle - passed
    
    * 1.5 Testing scenario - game with spare in the middle - failed
    
    * 1.5 Testing scenario - game with spare in the middle - passed
    
    * 1.6 and 1.7 testing scenarios - game with spare at the end and game with strike at the end - already working  - passed
    
    * 1.8 Testing scenario - game with all strikes - failed
    
    * Missing tests added from another PR - all passing
    
    * 1.8 Testing scenario - game with all strikes - passed. Mistakes from previous tests corrected
    
    * Unnecesary variables deleted
    
    * Added some missing testcases - all passed
    
    * Some testing scenarios added

--- bowling/player.cpp
+++ bowling/player.cpp
@@ -1,1 +1,32 @@
 #include "player.hpp"
+
+int Player::getScore() const {
+    return score_;
+}
+
+void Player::setScore(int score) {
+    score_ = score;
+}
+
+void Player::countScore(std::vector<std::pair<int, int>> score) {
+    int result = 0;
+
+    for (int i = 0; i <= 9; i++) {
+        if (score[i].first == 10) {
+            if (score[i + 1].first == 10) {
+                result += (score[i].first + score[i + 1].first + score[i + 2].first);
+            } else {
+                result += (score[i].first + score[i + 1].first + score[i + 1].second);
+            }
+            continue;
+        }
+
+        if ((score[i].first + score[i].second == 10) && (score[i].first != 10)) {
+            result += (score[i].first + score[i].second + score[i + 1].first);
+            continue;
+        }
+
+        result += (score[i].first + score[i].second);
+    }
+    Player::setScore(result);
+}
--- bowling/player.hpp
+++ bowling/player.hpp
@@ -1,3 +1,13 @@
 #pragma once
+#include <vector>
 
-class Player {};
+class Player {
+public:
+    int getScore() const;
+    void setScore(int score);
+
+    void countScore(std::vector<std::pair<int, int>> score);
+
+private:
+    int score_;
+};
