ccaa9f483a4ae03e6b50f5c871d934716b0e3ec5
majkel84 <michal.sobotka.84@gmail.com>
2020-08-12 22:00:27 +0200

    3.0 BowlingLane - start file edition

--- bowling/bowlingLane.hpp
+++ bowling/bowlingLane.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "file.hpp"
+#include "player.hpp"
+
 enum class gameStatus {
     NoGame,
     InProgress,
     Finish
 };
 
-class BowlingLane {};
+class BowlingLane {
+private:
+    gameStatus status_;
+    std::string name_;
+    std::unique_ptr<File> file_;
+    std::vector<std::unique_ptr<Player>> players_;
+};
