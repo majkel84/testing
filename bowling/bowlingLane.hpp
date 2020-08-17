9f2b410de61d0ee2526cb8eabdb9a464d15f17e0
majkel <michal.sobotka.84@gmail.com>
2020-08-17 11:37:26 +0200

    Zad3 (#33)
    * 0.2 add main-ut.cpp, new file added CMakeList udpated
    
    * 1.1 Test scenario Game of all misses, class Player getter and setter, struct TestPLayer
    
    * 1.1 Test scenario Game of all misses - test passed without count score implementatio
    
    * 1.2 Testing scenario game of all ones - test fail
    
    * 1.2 Testing scenario game of all ones - test pass
    
    * 1.3 Testing scenario game without spare and strike - test pass without any code correction
    
    * redundand filedeleted
    
    * 1.1 Test scenario Game of all misses - bug fixing
    
    * 4. Output file - testing scenario, experimental removed
    
    * 3.0 BowlingLane - enum class GameStatusadd to BwolingLane
    
    * 3.0 BowlingLane - start file edition
    
    * 3.0 BowlingLane - contructor for bowling lane

--- bowling/bowlingLane.hpp
+++ bowling/bowlingLane.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
-class BowlingLane {};
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "file.hpp"
+#include "player.hpp"
+
+enum class gameStatus {
+    NoGame,
+    InProgress,
+    Finish
+};
+
+class BowlingLane {
+public:
+    BowlingLane(File file);
+
+private:
+    gameStatus status_;
+    std::string name_;
+    std::unique_ptr<File> file_;
+    std::vector<std::unique_ptr<Player>> players_;
+};
