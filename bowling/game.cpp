6dddabfa09c6212a0d987363154cd30539bdaf09
Vicx95 <37795878+Vicx95@users.noreply.github.com>
2020-08-20 15:34:55 +0200

    Vicx95 (#34)
    * add static function printHelp
    
    * Implement printHelp function
    
    Co-authored-by: Mateusz Talma <talmamateusz@gmail.com>

--- bowling/game.cpp
+++ bowling/game.cpp
@@ -2,6 +2,15 @@
 
 #include <iostream>
 
-void Game::printHelp() {
-    std::cout << "MANUAL\n";
+void Game::printHelp(){
+    std::cout << "This is simple application which count points in Bowling Game \n"
+        << "Program takes two parameters:\n"
+        << "1. Is directory which includes txt files with game state"
+        << " on bowling lanes\n"
+        << "2.(optional) Is optional output file where processed results is saved\n"
+        << "If you don't give second parameter, results has been printed to screen\n"
+        << "Example of use: \n"
+        << "***** ./bowling inputDirectory results.txt\n"
+        << "***** ./bowling inputDirectory";
+
 }
