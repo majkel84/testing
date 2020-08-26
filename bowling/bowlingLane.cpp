c634dc3deca23e8d84013fc0a87350278c2343af
majkel <michal.sobotka.84@gmail.com>
2020-08-26 17:10:06 +0200

    Zad5 (#37)
    * 0.2 add main-ut.cpp, new file added CMakeList udpated
    
    * 1.1 Test scenario Game of all misses, class Player getter and setter, struct TestPLayer
    
    * 1.1 Test scenario Game of all misses - test passed without count score implementatio
    
    * 1.2 Testing scenario game of all ones - test fail
    
    * 1.2 Testing scenario game of all ones - test pass
    
    * 1.3 Testing scenario game without spare and strike - test pass without any code correction
    
    * redundand filedeleted
    
    * 1.1 Test scenario Game of all misses - bug fixing
    
    * 5. Show score - print and show methodes definition
    
    * 5. Show score - add field name to Player, definition of methodes print
    
    * 5. Show score - copy constructor for Player (input name and vector of points)
    
    * 5. Show score - method to set player in bowling lane
    
    * 5. Show score - getInfo method in Player
    
    * 5. Show score - getInfo method in Player updated - without point vector
    
    * 5. Show score - translateVectorToString in class Player
    
    * 5. Show score - translateVectorToString in class Player updated

--- bowling/bowlingLane.cpp
+++ bowling/bowlingLane.cpp
@@ -1,5 +1,24 @@
 #include "bowlingLane.hpp"
 
+#include <memory>
+#include <iostream>
+
 BowlingLane::BowlingLane(File file) {
     file = File(name_);
+}
+
+void BowlingLane::setPlayer() {
+    std::unique_ptr<Player> player
+            = std::make_unique<Player>("name", file_->getScores());
+    players_.emplace_back(std::move(player));
+}
+
+void BowlingLane::showResult() {
+    BowlingLane::printResultToScreen();
+}
+
+void BowlingLane::printResultToScreen() {
+    for (const auto& it : players_) {
+        std::cout << it->getInfo();
+    }
 }
