6652fa436eda4448edd9f5d4b4138a61f50a5313
majkel84 <michal.sobotka.84@gmail.com>
2020-08-27 17:19:53 +0200

    Zad5 Show Score - structure for tests in Bowling Lane

--- bowling/bowlingLane-ut.cpp
+++ bowling/bowlingLane-ut.cpp
@@ -1,3 +1,16 @@
 #include <gtest/gtest.h>
 
 #include "bowlingLane.hpp"
+
+struct TestBowlingLine : public ::testing::Test {
+    BowlingLane bl;
+    std::string results{};
+    std::vector<std::pair<int, int>> points {};
+};
+
+/*TEST_F(TestBowlingLine, checkIfGameInProgressIsTranslatedCorrectly) {
+    results = "15|25|45|13|";
+    score = {{1, 5}, {2, 5}, {4, 5}, {1, 3}};
+    file.translateResultsToScoreVector(results);
+    ASSERT_EQ(file.getScores(), score);
+}*/
